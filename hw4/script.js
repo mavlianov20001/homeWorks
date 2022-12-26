ccb32e65ecafe5cd85d02dd5288e2df41afb6220
Ali <112693871+mavlianov20001@users.noreply.github.com>
2022-12-26 21:47:49 +0600

    Add files via upload

---
+++ hw4/script.js
@@ -0,0 +1,54 @@
+// дз1
+//
+// let increment = document.querySelector(".increment");
+// let decrement = document.querySelector(".dekcrement");
+// let count = document.querySelector(".btnCountNumber");
+// let number = 0;
+// count.innerHTML = number;
+
+// increment.onclick = () => {
+//   number += 1;
+//   count.innerHTML = number;
+//   count.style.color = "green";
+// };
+
+// decrement.onclick = () => {
+//   number--;
+//   if (number >= 1) {
+//     count.innerHTML = number;
+//   } else if (number <= 0) {
+//     count.innerHTML = 0;
+//     number = 0;
+//   }
+//   count.style.color = "red";
+// };
+//
+//
+// дз 2
+// const posX = document.querySelector(".x");
+// const posY = document.querySelector(".y");
+// const blck = document.querySelector(".block");
+// blck.addEventListener("mousemove", (e) => {
+//   posX.innerHTML = e.offsetX;
+//   posY.innerHTML = e.offsetY;
+// });
+//
+//
+// дз 3
+
+// let color = prompt("вводите цвет !");
+// let r = document.querySelector(".red");
+// let y = document.querySelector(".yellow");
+// let g = document.querySelector(".green");
+// if (color == "красный") {
+//   alert("стоп !");
+//   r.style.background = "red";
+// } else if (color == "желтый") {
+//   alert("подожди !");
+//   y.style.background = "yellow";
+// } else if (color == "зеленный") {
+//   alert("езжай");
+//   g.style.background = "green";
+// } else {
+//   alert("надо вводить цвета !");
+// }
